cb6b724eaa7cf0f84612f71e588348f1237c19e2
pchuan <pchuan98@qq.com>
2023-06-19 17:22:08 +0800

    添加motor核心方法

--- Simscop.Spindisk/Simscop.Spindisk.Core/ViewModels/SteerViewModel.cs
+++ Simscop.Spindisk/Simscop.Spindisk.Core/ViewModels/SteerViewModel.cs
@@ -1,23 +1,88 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Simscop.API;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Simscop.Spindisk.Core.ViewModels;
 
 /// <summary>
 /// 舵机操作
 /// </summary>
-public partial class SteerViewModel:ObservableObject
+public partial class SteerViewModel : ObservableObject
 {
+    private readonly MshotMotor _motor = new();
+
+    private readonly DispatcherTimer _timer;
+
+    public SteerViewModel()
+    {
+
+        _timer = new DispatcherTimer(priority: DispatcherPriority.Background)
+        {
+            Interval = TimeSpan.FromSeconds(1),
+        };
+        Task.Run(() =>
+        {
+            IsConnected = _motor.InitializeMotor();
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        });
+    }
+
+    private void _timer_Tick(object? sender, EventArgs e)
+    {
+        X = _motor.X;
+        Y = _motor.Y;
+        Z = _motor.Z;
+
+        XEnable = _motor is { XEnabled: true, XAction: false, XException: false };
+        YEnable = _motor is { YEnabled: true, YAction: false, YException: false };
+        ZEnable = _motor is { ZEnabled: true, ZAction: false, ZException: false };
+    }
+
     [ObservableProperty]
     private double _x = 0;
+
+    [ObservableProperty]
+    private bool _xEnable = true;
 
     [ObservableProperty]
     private double _y = 0;
 
     [ObservableProperty]
+    private bool _yEnable = true;
+
+    [ObservableProperty]
     private double _z = 0;
+
+    [ObservableProperty]
+    private bool _zEnable = true;
+
+    [ObservableProperty]
+    private double _xyStep = 0;
+
+    [ObservableProperty]
+    private double _zStep = 0;
+
+    [ObservableProperty]
+    private bool _isConnected = false;
+
+    [RelayCommand]
+    void PositionToZero()
+        => _motor.ResetPosition();
+
+    public void MoveX(double step)
+        => _motor.SetXOffset(step);
+
+    public void MoveY(double step)  
+        => _motor.SetYOffset(step);
+
+    public void MoveZ(double step)
+        => _motor.SetZOffset(step);
+    
 }
--- Simscop.Spindisk/Simscop.Spindisk.WPF/Views/BaseSteerView.xaml.cs
+++ Simscop.Spindisk/Simscop.Spindisk.WPF/Views/BaseSteerView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Simscop.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Simscop.Spindisk.WPF.Views
 {
@@ -20,9 +23,113 @@ namespace Simscop.Spindisk.WPF.Views
     /// </summary>
     public partial class BaseSteerView : UserControl
     {
+        public const int TimerInterval = 100;
+
         public BaseSteerView()
         {
             InitializeComponent();
+            this.DataContext = null;
+            this.DataContextChanged += BaseSteerView_DataContextChanged;
+
+
+        }
+
+        private void BaseSteerView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is not BaseSteerView view) return;
+            if (view.DataContext is not Core.ViewModels.SteerViewModel vm) return;
+            
+
+            var btList = new List<IconButton>()
+            {
+                RightMoveBt,RightBottomMoveBt,
+                BottomMoveBt,LeftBottomMoveBt,
+                LeftMoveBt,LeftTopMoveBt,
+                TopMoveBt,RightTopMoveBt,
+                UpMoveBt,DownMoveBt
+            };
+
+            var actions = new List<Action<double>>
+            {
+                MoveX,
+
+                v =>
+                {
+                    MoveX(v);
+                    MoveY(-v);
+                },
+
+                v=>MoveY(-v),
+
+                v =>
+                {
+                    MoveX(-v);
+                    MoveY(-v);
+                },
+
+                v => MoveX(-v),
+
+                v =>
+                {
+                    MoveX(-v);
+                    MoveY(v);
+                },
+
+                MoveY,
+
+                v =>
+                {
+                    MoveX(v);
+                    MoveY(v);
+                },
+            };
+
+            var timers = new List<DispatcherTimer>();
+
+            var valList = new List<double>()
+            {
+                vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.ZStep,vm.ZStep
+            };
+
+            for (var i = 0; i < 10; i++)
+            {
+                timers[i] = new DispatcherTimer(priority: DispatcherPriority.Render)
+                {
+                    Interval = TimeSpan.FromMilliseconds(TimerInterval),
+                };
+
+                var index = i;
+                timers[i].Tick += (s, args) =>
+                    actions[index](valList[index]);
+
+                btList[i].PreviewMouseLeftButtonDown += (s, args) =>
+                    timers[index].Start();
+                
+                btList[i].PreviewMouseLeftButtonUp += (s, args) =>
+                    timers[index].Stop();
+                
+            }
+        }
+
+        private void MoveX(double value)
+        {
+            if (DataContext is not Core.ViewModels.SteerViewModel vm) return;
+
+            vm.MoveX(value);
+        }
+
+        private void MoveY(double value)
+        {
+            if (DataContext is not Core.ViewModels.SteerViewModel vm) return;
+
+            vm.MoveY(value);
+        }
+
+        private void MoveZ(double value)
+        {
+            if (DataContext is not Core.ViewModels.SteerViewModel vm) return;
+
+            vm.MoveZ(value);
         }
     }
 }

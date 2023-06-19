e400f5f5df2d2e0256807a6e632e126e205d5f51
pchuan <pchuan98@qq.com>
2023-06-19 17:52:49 +0800

    test

--- Simscop.Spindisk/Simscop.Spindisk.WPF/Views/BaseSteerView.xaml.cs
+++ Simscop.Spindisk/Simscop.Spindisk.WPF/Views/BaseSteerView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Simscop.UI.Controls;
+﻿using Simscop.Spindisk.Core.ViewModels;
+using Simscop.UI.Controls;
 using System;
 using System.Collections.Generic;
+using System.Printing.IndexedProperties;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
@@ -13,6 +15,9 @@ namespace Simscop.Spindisk.WPF.Views
     public partial class BaseSteerView : UserControl
     {
         public const int TimerInterval = 100;
+        private readonly DispatcherTimer _rightTimer;
+
+        public SteerViewModel? Vm => DataContext as SteerViewModel;
 
         public BaseSteerView()
         {
@@ -20,82 +25,92 @@ namespace Simscop.Spindisk.WPF.Views
             this.DataContext = null;
             this.DataContextChanged += BaseSteerView_DataContextChanged;
 
+            _rightTimer = new DispatcherTimer(priority: DispatcherPriority.Render)
+            {
+                Interval = TimeSpan.FromMilliseconds(TimerInterval)
+            };
+
+            _rightTimer.Tick += (s, e) => Vm?.MoveX(Vm.XyStep);
+
+            this.RightMoveBt.PreviewMouseDown += (s, e) => _rightTimer.Stop(); ;
+            this.RightMoveBt.PreviewMouseUp += (s, e) => _rightTimer.Start(); 
+
 
         }
 
         private void BaseSteerView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (sender is not BaseSteerView view) return;
-            if (view.DataContext is not Core.ViewModels.SteerViewModel vm) return;
-            
+            //if (sender is not BaseSteerView view) return;
+            //if (view.DataContext is not Core.ViewModels.SteerViewModel vm) return;
 
-            var btList = new List<IconButton>()
-            {
-                RightMoveBt,RightBottomMoveBt,
-                BottomMoveBt,LeftBottomMoveBt,
-                LeftMoveBt,LeftTopMoveBt,
-                TopMoveBt,RightTopMoveBt,
-                UpMoveBt,DownMoveBt
-            };
 
-            var actions = new List<Action<double>>
-            {
-                MoveX,
+            //var btList = new List<IconButton>()
+            //{
+            //    RightMoveBt,RightBottomMoveBt,
+            //    BottomMoveBt,LeftBottomMoveBt,
+            //    LeftMoveBt,LeftTopMoveBt,
+            //    TopMoveBt,RightTopMoveBt,
+            //    UpMoveBt,DownMoveBt
+            //};
 
-                v =>
-                {
-                    MoveX(v);
-                    MoveY(-v);
-                },
+            //var actions = new List<Action<double>>
+            //{
+            //    MoveX,
 
-                v=>MoveY(-v),
+            //    v =>
+            //    {
+            //        MoveX(v);
+            //        MoveY(-v);
+            //    },
 
-                v =>
-                {
-                    MoveX(-v);
-                    MoveY(-v);
-                },
+            //    v=>MoveY(-v),
 
-                v => MoveX(-v),
+            //    v =>
+            //    {
+            //        MoveX(-v);
+            //        MoveY(-v);
+            //    },
 
-                v =>
-                {
-                    MoveX(-v);
-                    MoveY(v);
-                },
+            //    v => MoveX(-v),
 
-                MoveY,
+            //    v =>
+            //    {
+            //        MoveX(-v);
+            //        MoveY(v);
+            //    },
 
-                v =>
-                {
-                    MoveX(v);
-                    MoveY(v);
-                },
-            };
+            //    MoveY,
 
-            var valList = new List<double>()
-            {
-                vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.ZStep,vm.ZStep
-            };
+            //    v =>
+            //    {
+            //        MoveX(v);
+            //        MoveY(v);
+            //    },
+            //};
 
-            for (var i = 0; i < 10; i++)
-            {
-                var timer = new DispatcherTimer(priority: DispatcherPriority.Render)
-                {
-                    Interval = TimeSpan.FromMilliseconds(TimerInterval),
-                };
+            //var valList = new List<double>()
+            //{
+            //    vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.XyStep,vm.ZStep,vm.ZStep
+            //};
 
-                var index = i;
-                timer.Tick += (s, args) =>
-                    actions[index](valList[index]);
+            //for (var i = 0; i < 10; i++)
+            //{
+            //    var timer = new DispatcherTimer(priority: DispatcherPriority.Render)
+            //    {
+            //        Interval = TimeSpan.FromMilliseconds(TimerInterval),
+            //    };
 
-                btList[i].PreviewMouseLeftButtonDown += (s, args) =>
-                    timer.Start();
-                
-                btList[i].PreviewMouseLeftButtonUp += (s, args) =>
-                    timer.Stop();
-                
-            }
+            //    var index = i;
+            //    timer.Tick += (s, args) =>
+            //        actions[index](valList[index]);
+
+            //    btList[i].PreviewMouseLeftButtonDown += (s, args) =>
+            //        timer.Start();
+
+            //    btList[i].PreviewMouseLeftButtonUp += (s, args) =>
+            //        timer.Stop();
+
+            //}
         }
 
         private void MoveX(double value)
